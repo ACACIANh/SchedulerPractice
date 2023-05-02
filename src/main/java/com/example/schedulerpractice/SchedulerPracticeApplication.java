cfe94d89acd53520d428bd880a03533d75813976
ACACIANha <gkehdrn95@netand.co.kr>
2023-05-02 11:12:38 +0900

    init

---
+++ src/main/java/com/example/schedulerpractice/SchedulerPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedulerpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerPracticeApplication {
+
+	public static void main( String[] args ) {
+		SpringApplication.run( SchedulerPracticeApplication.class, args );
+	}
+
+}
